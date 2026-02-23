03b062cc3da8d5175803990bb528c80e03772912
EphriamHab <ephremhabtamu2015@gmail.com>
2026-02-23 15:57:11 +0300

    feat: add validation schemas for user registration, login, and transactions

---
+++ validators/transaction.validator.js
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+export const transactionSchema = z.object({
+  amount: z.number().positive(),
+  type: z.enum(["income", "expense"]),
+  category: z.string(),
+  description: z.string(),
+  date: z.string(),
+  userId: z.string(),
+});
