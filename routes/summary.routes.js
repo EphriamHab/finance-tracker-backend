90b318705c81e02ba9bd538971f594103ccc6295
Ephrem Habtamu <117913070+EphriamHab@users.noreply.github.com>
2026-02-26 11:16:45 +0300

    Update summary.routes.js

--- routes/summary.routes.js
+++ routes/summary.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { getSummary } from "../controllers/summary.controller.js";
+import { authMiddleware } from "../middleware/auth.middleware.js";
+
 
 const router = Router();
 
-router.get("/", getSummary);
+router.get("/", authMiddleware, getSummary);
 
 export default router;
