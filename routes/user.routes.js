581bf1b5fc6348a211cc6c61ea7eef0cc2a094b4
Ephrem Habtamu <117913070+EphriamHab@users.noreply.github.com>
2026-02-26 10:55:30 +0300

    Add auth middleware to user routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { getUser, getCurrentUser, getUsers } from "../controllers/user.controller.js";
+import { authMiddleware } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.get("/", getUsers);
-router.get("/me", getCurrentUser);
-router.get("/:id", getUser);
+router.get("/", authMiddleware, getUsers);
+router.get("/me", authMiddleware, getCurrentUser);
+router.get("/:id", authMiddleware, getUser);
 
 export default router;
